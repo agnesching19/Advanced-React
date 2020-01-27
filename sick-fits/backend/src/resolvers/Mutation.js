c45e82649341f308e17d23f9bfd114d75a977f7a
Agnes Ching <agnesching19@gmail.com>
2020-01-27 22:05:12 +0000

    Delete items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -20,7 +20,16 @@ const Mutations = {
         id: args.id
       }
     }, info);
-  }
+  },
+  async deleteItem(parent, args, context, info) {
+    const where = { id: args.id };
+    // Find the item
+    const item = await context.db.query.item({ where }, `{ id title }`);
+    // Check if they own the item, or have the permission
+    // TODO
+    // Delete it!
+    return context.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = Mutations;
