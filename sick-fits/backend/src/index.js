0c2639d0f264db0d9808c1a3ce9f02aec6167530
Agnes Ching <agnesching19@gmail.com>
2020-01-30 22:02:00 +0000

    Currently login user with middlewear'

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,6 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -6,7 +8,17 @@ const db = require('./db');
 const server = createServer();
 
 server.express.use(cookieParser());
-// TODO Use exprese middleware to populate current user
+
+// Decode the jwt so we can get the user id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { user } = jwt.verify(token, process.env.APP_SECRET);
+    // Put the userId onto the req for future requests to access
+    req.userId = user;
+  }
+  next();
+});
 
 server.start({
   cors: {
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -4,10 +4,18 @@ const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
-  // async items(parent, args, context, info) {
-  //   const allItems = await context.db.query.items();
-  //   return allItems;
-  // }
+  me(parent, args, ctx, info) {
+    // Check if there is a current user ID
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;
