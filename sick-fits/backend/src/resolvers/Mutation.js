60d8fdc39be72e11e2f0acfa42833c4726fc3316
Agnes Ching <agnesching19@gmail.com>
2020-02-09 18:10:33 +0000

    Remove cart items'

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -223,6 +223,26 @@ const Mutations = {
         },
       }
     }, info);
+  },
+  async removeFromCart(parent, args, context, info) {
+    // Find the cart item
+    const cartItem = await context.db.query.cartItem({
+      where: {
+        id: args.id,
+      }
+    }, `{ id, user { id }}`);
+    // Make sure we find an item
+    if (!cartItem) {
+      throw new Error('No CartItem Found!');
+    }
+    // Make sure they own that cart item
+    if (cartItem.user.id !== context.request.userId) {
+      throw new Error("Don't ever try to do that!");
+    }
+    // Delete that cart item
+    return context.db.mutation.deleteCartItem({
+      where: { id: args.id }
+    }, info);
   }
 };
 
