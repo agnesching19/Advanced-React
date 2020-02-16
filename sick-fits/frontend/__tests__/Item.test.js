0d4c860c47ae56d247a2504adca845b17f4514e1
Agnes Ching <agnesching19@gmail.com>
2020-02-16 16:59:01 +0000

    Shallow rendering

---
+++ sick-fits/frontend/__tests__/Item.test.js
@@ -0,0 +1,36 @@
+import ItemComponent from '../components/Item';
+import { shallow } from 'enzyme';
+
+const fakeItem = {
+  id: 'ABC123',
+  title: 'The Best Whisky',
+  price: 99999,
+  description: 'The best whisky in the world that you should try!',
+  image: 'whisky.jpg',
+  largeImage: 'largeWhisky.jpg'
+}
+
+describe('<Item />', () => {
+  it('renders the PriceTag and title properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const PriceTag = wrapper.find('PriceTag');
+    expect(PriceTag.children().text()).toBe('£999.99');
+    expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+  });
+
+  it('renders the image properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const image = wrapper.find('img');
+    expect(image.props().src).toBe(fakeItem.image);
+    expect(image.props().alt).toBe(fakeItem.title);
+  });
+
+  it('renders out the buttons properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const buttonList = wrapper.find('.buttonList');
+    expect(buttonList.children()).toHaveLength(3);
+    expect(buttonList.find('Link')).toHaveLength(1);
+    expect(buttonList.find('AddToCart')).toBeTruthy();
+    expect(buttonList.find('DeleteItem').exists()).toBe(true);
+  });
+});
